c6ed9c1ea5b67d882950c8b1e8a668a92fb92ab2
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-16 16:35:14 +0000

    style(tab-menu): Add width % handling below 576px

--- packages/pancake-uikit/src/components/TabMenu/StyledTab.tsx
+++ packages/pancake-uikit/src/components/TabMenu/StyledTab.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 interface StyledTabProps {
   color: "card" | "textSubtle";
   bgColor: "textSubtle" | "input";
+  tabWidthPercentage?: number;
 }
 
 const StyledTab = styled.button<StyledTabProps>`
@@ -11,11 +12,17 @@ const StyledTab = styled.button<StyledTabProps>`
   cursor: pointer;
   border: 0;
   outline: 0;
-  min-width: 84px;
+  width: ${({ tabWidthPercentage }) => `${tabWidthPercentage}%`};
+
+  min-width: 83px;
   padding: 8px 0;
   border-radius: 16px 16px 0 0;
   color: ${({ theme, color }) => theme.colors[color]};
   background-color: ${({ theme, bgColor }) => theme.colors[bgColor]};
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    width: auto;
+  }
 `;
 
 export default StyledTab;
--- packages/pancake-uikit/src/components/TabMenu/Tab.tsx
+++ packages/pancake-uikit/src/components/TabMenu/Tab.tsx
@@ -3,11 +3,20 @@ import StyledTab from "./StyledTab";
 import { TabProps } from "./types";
 import { Text } from "../Text";
 
-const Tab: React.FC<TabProps> = ({ isActive = false, onClick, children }) => (
-  <StyledTab onClick={onClick} bgColor={isActive ? "textSubtle" : "input"} color={isActive ? "card" : "textSubtle"}>
-    <Text fontWeight={600} color={isActive ? "card" : "textSubtle"} m="0 8px">
-      {children}
-    </Text>
-  </StyledTab>
-);
+const Tab: React.FC<TabProps> = ({ isActive = false, onClick, tabMenuLength = 1, children }) => {
+  const tabWidthPercentage = 100 / tabMenuLength;
+
+  return (
+    <StyledTab
+      onClick={onClick}
+      tabWidthPercentage={tabWidthPercentage}
+      bgColor={isActive ? "textSubtle" : "input"}
+      color={isActive ? "card" : "textSubtle"}
+    >
+      <Text fontWeight={600} color={isActive ? "card" : "textSubtle"} m="0 8px">
+        {children}
+      </Text>
+    </StyledTab>
+  );
+};
 export default Tab;
