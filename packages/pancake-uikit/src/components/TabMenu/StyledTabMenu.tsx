1a4d0a63bf0f9b1b734d5e2b8592e5c58529be59
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-16 15:24:36 +0000

    chore(tab-menu): Remove unused commented code

--- packages/pancake-uikit/src/components/TabMenu/StyledTabMenu.tsx
+++ packages/pancake-uikit/src/components/TabMenu/StyledTabMenu.tsx
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import Flex from "../Box/Flex";
 
 const StyledTabMenu = styled(Flex)`
-  /* display: inline-flex; */
   border-bottom: 2px solid ${({ theme }) => theme.colors.textSubtle};
   padding: 0 16px;
 
