a670ba7061f7961cee4e3dc69e01825e4ef50585
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-16 14:58:30 +0000

    test(tab-menu): Added snapshot

--- packages/pancake-uikit/src/components/TabMenu/types.ts
+++ packages/pancake-uikit/src/components/TabMenu/types.ts
@@ -1,10 +1,13 @@
-export interface TabMenuItemProps {
-  isActive?: boolean;
-  onClick?: () => void;
+export interface StyledTabProps {
+  color: "card" | "textSubtle";
+  bgColor: "textSubtle" | "input";
 }
-
 export interface TabMenuProps {
   activeIndex?: number;
   onItemClick?: (index: number) => void;
   children: React.ReactElement[];
+}
+export interface TabMenuItemProps {
+  isActive?: boolean;
+  onClick?: () => void;
 }
