f6cbad1c919a907a4116eaa3106b6af6f23a1186
ChefHutch <chef.hutch.pcs@protonmail.com>
2021-03-16 12:37:27 +0000

    style(styled-tab): Refinding styles and element types

--- packages/pancake-uikit/src/components/TabMenu/StyledTabMenu.tsx
+++ packages/pancake-uikit/src/components/TabMenu/StyledTabMenu.tsx
@@ -4,6 +4,10 @@ import Flex from "../Box/Flex";
 const StyledTabMenu = styled(Flex)`
   display: inline-flex;
   border-bottom: 2px solid ${({ theme }) => theme.colors.textSubtle};
+
+  & > div + div {
+    margin-left: 2px; // To avoid focus shadow overlap
+  }
 `;
 
 export default StyledTabMenu;
