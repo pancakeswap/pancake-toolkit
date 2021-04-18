df4f4e9746c3236c2e72ce5129e5a9c72c34f9b3
memoyil <2213635+memoyil@users.noreply.github.com>
2021-04-18 20:36:27 +0200

    fix: Add clear websocket

--- packages/pancake-uikit/src/widgets/WalletModal/AccountModal.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/AccountModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Button from "../../components/Button/Button";
 import Text from "../../components/Text/Text";
 import LinkExternal from "../../components/Link/LinkExternal";
@@ -14,36 +14,46 @@ interface Props {
   onDismiss?: () => void;
 }
 
-const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null }) => (
-  <Modal title="Your wallet" onDismiss={onDismiss}>
-    <Text
-      fontSize="20px"
-      bold
-      style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" }}
-    >
-      {account}
-    </Text>
-    <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
-        View on BscScan
-      </LinkExternal>
-      <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
-    </Flex>
-    <Flex justifyContent="center">
-      <Button
-        scale="sm"
-        variant="secondary"
-        onClick={() => {
-          logout();
-          window.localStorage.removeItem(connectorLocalStorageKey);
-          window.localStorage.removeItem(ConnectorNames.WalletConnect);
-          onDismiss();
-        }}
+const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null }) => {
+  const clearCache = useCallback(() => {
+    window.localStorage.removeItem(connectorLocalStorageKey);
+    if (window.localStorage.getItem(ConnectorNames.WalletConnect)) {
+      window.localStorage.removeItem(ConnectorNames.WalletConnect);
+      // Reload the webpage to disconnect walletconnect websocket.
+      window.location.reload();
+    }
+  }, []);
+
+  return (
+    <Modal title="Your wallet" onDismiss={onDismiss}>
+      <Text
+        fontSize="20px"
+        bold
+        style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" }}
       >
-        Logout
-      </Button>
-    </Flex>
-  </Modal>
-);
+        {account}
+      </Text>
+      <Flex mb="32px">
+        <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
+          View on BscScan
+        </LinkExternal>
+        <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
+      </Flex>
+      <Flex justifyContent="center">
+        <Button
+          scale="sm"
+          variant="secondary"
+          onClick={() => {
+            logout();
+            clearCache();
+            onDismiss();
+          }}
+        >
+          Logout
+        </Button>
+      </Flex>
+    </Modal>
+  );
+};
 
 export default AccountModal;
