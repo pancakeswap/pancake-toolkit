e132006eb38bf5202111e1667f9d811ca2e5088a
Chef-Cheems <chefcheems@protonmail.com>
2021-04-19 10:52:22 +0300

    fix: Export GetProfileResponse type

--- packages/pancake-profile-sdk/src/index.ts
+++ packages/pancake-profile-sdk/src/index.ts
@@ -1,5 +1,5 @@
 export { default as getUsername } from "./functions/getUsername";
-export { default as getProfile } from "./functions/getProfile";
+export { default as getProfile, GetProfileResponse } from "./functions/getProfile";
 export { default as getTeam } from "./functions/getTeam";
 export { default as getAchievements } from "./functions/getAchievements";
 export * from "./types";
