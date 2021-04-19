9ca408e4ea68943d3aecf721db97dd0f7afebe4b
Chef-Cheems <chefcheems@protonmail.com>
2021-04-19 10:44:10 +0300

    fix: Update exports

--- packages/pancake-profile-sdk/src/index.ts
+++ packages/pancake-profile-sdk/src/index.ts
@@ -1,6 +1,5 @@
-import getUsername from "./functions/getUsername";
-import getProfile from "./functions/getProfile";
-import getTeam from "./functions/getTeam";
-import getAchievements from "./functions/getAchievements";
-
-export { getUsername, getProfile, getTeam, getAchievements };
+export { default as getUsername } from "./functions/getUsername";
+export { default as getProfile } from "./functions/getProfile";
+export { default as getTeam } from "./functions/getTeam";
+export { default as getAchievements } from "./functions/getAchievements";
+export * from "./types";
