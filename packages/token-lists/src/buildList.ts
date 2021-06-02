f9dc6aeb538ef968f6a5fcaf85082856e154d9ee
Chef Cheems <chefcheems@protonmail.com>
2021-06-02 11:17:05 +0300

    fix: Make CAKE first in extended, generate lists

--- packages/token-lists/src/buildList.ts
+++ packages/token-lists/src/buildList.ts
@@ -41,6 +41,10 @@ export const buildList = (listName: string): TokenList => {
     tokens: sort
       ? list.sort((t1, t2) => {
           if (t1.chainId === t2.chainId) {
+            // CAKE first in extended list
+            if ((t1.symbol === "CAKE") !== (t2.symbol === "CAKE")) {
+              return t1.symbol === "CAKE" ? -1 : 1;
+            }
             return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
           }
           return t1.chainId < t2.chainId ? -1 : 1;
