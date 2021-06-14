04ce219dce7b4ef0727a466ed691a100ac54744e
Chef Chungus <71833681+Chef-Chungus@users.noreply.github.com>
2021-06-14 13:18:22 +0900

    chore: Add support for PCS top 15 & update blacklist for top 100

--- packages/token-lists/src/index.ts
+++ packages/token-lists/src/index.ts
@@ -1,6 +1,7 @@
 import { buildList, saveList } from "./buildList";
 import checksumAddresses from "./checksum";
-import topTokens from "./top-100";
+import top100Tokens from "./top-100";
+import top15Tokens from "./top-15";
 
 const command = process.argv[2];
 const listName = process.argv[3];
@@ -13,7 +14,8 @@ switch (command) {
     saveList(buildList(listName), listName);
     break;
   case "fetch":
-    topTokens();
+    top100Tokens();
+    top15Tokens();
     break;
   default:
     console.info("Unknown command");
