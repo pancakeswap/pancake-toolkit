a54b3b919f4c2ca86986e50a81bb30488deadba7
Chef Cheems <chefcheems@protonmail.com>
2021-06-17 10:33:20 +0300

    feat: Add achievement badge images

--- packages/pancake-profile-sdk/src/images/index.ts
+++ packages/pancake-profile-sdk/src/images/index.ts
@@ -1,3 +1,53 @@
+// Achievements
+import easterChampionBronze from "./achievements/easter-champion-bronze.svg";
+import easterChampionGold from "./achievements/easter-champion-gold.svg";
+import easterChampionSilver from "./achievements/easter-champion-silver.svg";
+
+import easterParticipantBronze from "./achievements/easter-participant-bronze.svg";
+import easterParticipantGold from "./achievements/easter-participant-gold.svg";
+import easterParticipantSilver from "./achievements/easter-participant-silver.svg";
+
+import easterTop500Bronze from "./achievements/easter-top-500-bronze.svg";
+import easterTop500Gold from "./achievements/easter-top-500-gold.svg";
+import easterTop500Silver from "./achievements/easter-top-500-silver.svg";
+
+import ifoBelt from "./achievements/ifo-belt.svg";
+import ifoBlk from "./achievements/ifo-blk.svg";
+import ifoBry from "./achievements/ifo-bry.svg";
+import ifoDitto from "./achievements/ifo-ditto.svg";
+import ifoHelmet from "./achievements/ifo-helmet.svg";
+import ifoHotcross from "./achievements/ifo-hotcross.svg";
+import ifoHzn from "./achievements/ifo-hzn.svg";
+import ifoKalm from "./achievements/ifo-kalm.svg";
+import ifoTen from "./achievements/ifo-ten.svg";
+import ifoWatch from "./achievements/ifo-watch.svg";
+import ifoWsote from "./achievements/ifo-wsote.svg";
+
+export const ifoBadges = {
+  "easter-champion-bronze.svg": easterChampionBronze,
+  "easter-champion-gold.svg": easterChampionGold,
+  "easter-champion-silver.svg": easterChampionSilver,
+  "easter-participant-bronze.svg": easterParticipantBronze,
+  "easter-participant-gold.svg": easterParticipantGold,
+  "easter-participant-silver.svg": easterParticipantSilver,
+  "easter-top-500-bronze.svg": easterTop500Bronze,
+  "easter-top-500-gold.svg": easterTop500Gold,
+  "easter-top-500-silver.svg": easterTop500Silver,
+  "ifo-belt.svg": ifoBelt,
+  "ifo-blk.svg": ifoBlk,
+  "ifo-bry.svg": ifoBry,
+  "ifo-ditto.svg": ifoDitto,
+  "ifo-helmet.svg": ifoHelmet,
+  "ifo-hotcross.svg": ifoHotcross,
+  "ifo-hzn.svg": ifoHzn,
+  "ifo-kalm.svg": ifoKalm,
+  "ifo-ten.svg": ifoTen,
+  "ifo-watch.svg": ifoWatch,
+  "ifo-wsote.svg": ifoWsote,
+};
+
+// Team images
+
 export { default as chaoticCakersAlt } from "./teams/chaotic-cakers-alt.png";
 export { default as chaoticCakersSm } from "./teams/chaotic-cakers-sm.png";
 export { default as chaoticCakersMd } from "./teams/chaotic-cakers-md.png";
