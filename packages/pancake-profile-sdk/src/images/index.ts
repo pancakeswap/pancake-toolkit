4af718a06da1016078f640a68e3bd760670e1485
Chef Cheems <chefcheems@protonmail.com>
2021-06-17 10:16:52 +0300

    chore: Move team images to separate direcotry

--- packages/pancake-profile-sdk/src/images/index.ts
+++ packages/pancake-profile-sdk/src/images/index.ts
@@ -1,17 +1,17 @@
-export { default as chaoticCakersAlt } from "./chaotic-cakers-alt.png";
-export { default as chaoticCakersSm } from "./chaotic-cakers-sm.png";
-export { default as chaoticCakersMd } from "./chaotic-cakers-md.png";
-export { default as chaoticCakersLg } from "./chaotic-cakers-lg.png";
-export { default as chaoticCakersBg } from "./chaotic-cakers-bg.svg";
+export { default as chaoticCakersAlt } from "./teams/chaotic-cakers-alt.png";
+export { default as chaoticCakersSm } from "./teams/chaotic-cakers-sm.png";
+export { default as chaoticCakersMd } from "./teams/chaotic-cakers-md.png";
+export { default as chaoticCakersLg } from "./teams/chaotic-cakers-lg.png";
+export { default as chaoticCakersBg } from "./teams/chaotic-cakers-bg.svg";
 
-export { default as fearsomeFlippersAlt } from "./fearsome-flippers-alt.png";
-export { default as fearsomeFlippersSm } from "./fearsome-flippers-sm.png";
-export { default as fearsomeFlippersMd } from "./fearsome-flippers-md.png";
-export { default as fearsomeFlippersLg } from "./fearsome-flippers-lg.png";
-export { default as fearsomeFlippersBg } from "./fearsome-flippers-bg.svg";
+export { default as fearsomeFlippersAlt } from "./teams/fearsome-flippers-alt.png";
+export { default as fearsomeFlippersSm } from "./teams/fearsome-flippers-sm.png";
+export { default as fearsomeFlippersMd } from "./teams/fearsome-flippers-md.png";
+export { default as fearsomeFlippersLg } from "./teams/fearsome-flippers-lg.png";
+export { default as fearsomeFlippersBg } from "./teams/fearsome-flippers-bg.svg";
 
-export { default as syrupStormAlt } from "./syrup-storm-alt.png";
-export { default as syrupStormSm } from "./syrup-storm-sm.png";
-export { default as syrupStormMd } from "./syrup-storm-md.png";
-export { default as syrupStormLg } from "./syrup-storm-lg.png";
-export { default as syrupStormBg } from "./syrup-storm-bg.svg";
+export { default as syrupStormAlt } from "./teams/syrup-storm-alt.png";
+export { default as syrupStormSm } from "./teams/syrup-storm-sm.png";
+export { default as syrupStormMd } from "./teams/syrup-storm-md.png";
+export { default as syrupStormLg } from "./teams/syrup-storm-lg.png";
+export { default as syrupStormBg } from "./teams/syrup-storm-bg.svg";
