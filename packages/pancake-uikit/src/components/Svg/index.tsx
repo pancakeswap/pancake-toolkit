ef649afca8634bc968eb36e0c6b47ff393cba94f
hachiojidev <hachiojidev@protonmail.com>
2021-07-14 16:21:28 +0900

    feat(uikit): Binance Chain icon

--- packages/pancake-uikit/src/components/Svg/index.tsx
+++ packages/pancake-uikit/src/components/Svg/index.tsx
@@ -9,6 +9,7 @@ export { default as ArrowLastIcon } from "./Icons/ArrowLast";
 export { default as ArrowUpIcon } from "./Icons/ArrowUp";
 export { default as AutoRenewIcon } from "./Icons/AutoRenew";
 export { default as BinanceIcon } from "./Icons/Binance";
+export { default as BinanceChainIcon } from "./Icons/BinanceChain";
 export { default as BlockIcon } from "./Icons/Block";
 export { default as BnbUsdtPairTokenIcon } from "./Icons/BnbUsdtPairToken";
 export { default as BunnyCardsIcon } from "./Icons/BunnyCards";
