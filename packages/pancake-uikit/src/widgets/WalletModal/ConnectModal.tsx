3bfba660e38efb92bc9376cdbc81cb0fb63e7785
hachiojidev <hachiojidev@protonmail.com>
2021-07-15 10:00:06 +0900

    feat(uikit): Wallet connect revamp

--- packages/pancake-uikit/src/widgets/WalletModal/ConnectModal.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/ConnectModal.tsx
@@ -1,40 +1,70 @@
-import React from "react";
-import styled from "styled-components";
-import { Link } from "../../components/Link";
-import { HelpIcon } from "../../components/Svg";
-import { Modal } from "../Modal";
-import WalletCard from "./WalletCard";
+import React, { useState } from "react";
+import styled, { useTheme } from "styled-components";
+import Grid from "../../components/Box/Grid";
+import Box from "../../components/Box/Box";
+import getThemeValue from "../../util/getThemeValue";
+import Text from "../../components/Text/Text";
+import Heading from "../../components/Heading/Heading";
+import { Button } from "../../components/Button";
+import { ModalBody, ModalCloseButton, ModalContainer, ModalHeader, ModalTitle } from "../Modal";
+import WalletCard, { MoreWalletCard } from "./WalletCard";
 import config from "./config";
 import { Login } from "./types";
 
 interface Props {
   login: Login;
   onDismiss?: () => void;
+  displayCount?: number;
 }
 
-const HelpLink = styled(Link)`
-  display: flex;
-  align-self: center;
-  align-items: center;
-  margin-top: 24px;
+const WalletWrapper = styled(Box)`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.cardBorder};
 `;
 
-const ConnectModal: React.FC<Props> = ({ login, onDismiss = () => null }) => (
-  <Modal title="Connect to a wallet" onDismiss={onDismiss}>
-    {config.map((entry, index) => (
-      <WalletCard
-        key={entry.title}
-        login={login}
-        walletConfig={entry}
-        onDismiss={onDismiss}
-        mb={index < config.length - 1 ? "8px" : "0"}
-      />
-    ))}
-    <HelpLink href="https://docs.pancakeswap.finance/get-started/connection-guide" external>
-      <HelpIcon color="primary" mr="6px" />
-      Learn how to connect
-    </HelpLink>
-  </Modal>
-);
+const sortedConfig = config.sort((a, b) => a.priority - b.priority);
+
+const ConnectModal: React.FC<Props> = ({ login, onDismiss = () => null, displayCount = 3 }) => {
+  const [showMore, setShowMore] = useState(false);
+  const theme = useTheme();
+  const displayListConfig = showMore ? sortedConfig : sortedConfig.slice(0, displayCount);
+
+  return (
+    <ModalContainer minWidth="320px">
+      <ModalHeader background={getThemeValue("colors.gradients.bubblegum")(theme)}>
+        <ModalTitle>
+          <Heading>Connect Wallet</Heading>
+        </ModalTitle>
+        <ModalCloseButton onDismiss={onDismiss} />
+      </ModalHeader>
+      <ModalBody width={["320px", null, "340px"]}>
+        <WalletWrapper py="24px" maxHeight="394px" overflowY="auto">
+          <Grid gridTemplateColumns="1fr 1fr" gridGap="8px">
+            {displayListConfig.map((wallet) => (
+              <Box key={wallet.title}>
+                <WalletCard walletConfig={wallet} login={login} onDismiss={onDismiss} />
+              </Box>
+            ))}
+            {!showMore && <MoreWalletCard onClick={() => setShowMore(true)} />}
+          </Grid>
+        </WalletWrapper>
+        <Box p="24px">
+          <Text textAlign="center" color="textSubtle" as="p" mb="16px">
+            Haven&#39;t got a crypto wallet yet?
+          </Text>
+          <Button
+            as="a"
+            href="https://docs.pancakeswap.finance/get-started/connection-guide"
+            variant="subtle"
+            width="100%"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Learn How to Connect
+          </Button>
+        </Box>
+      </ModalBody>
+    </ModalContainer>
+  );
+};
 
 export default ConnectModal;
--- packages/pancake-uikit/src/widgets/WalletModal/WalletCard.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/WalletCard.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 import Button from "../../components/Button/Button";
 import Text from "../../components/Text/Text";
+import MoreVert from "../../components/Svg/Icons/MoreVert";
+import { ButtonProps } from "../../components/Button";
 import { connectorLocalStorageKey } from "./config";
 import { Login, Config } from "./types";
 
@@ -8,29 +11,46 @@ interface Props {
   walletConfig: Config;
   login: Login;
   onDismiss: () => void;
-  mb: string;
 }
 
-const WalletCard: React.FC<Props> = ({ login, walletConfig, onDismiss, mb }) => {
+const WalletButton = styled(Button).attrs({ width: "100%", variant: "text", py: "16px" })`
+  align-items: center;
+  display: flex;
+  height: 80px;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const MoreWalletCard: React.FC<ButtonProps> = (props) => {
+  return (
+    <WalletButton variant="tertiary" {...props}>
+      <MoreVert width="40px" />
+      <Text bold color="textSubtle" fontSize="14px">
+        More
+      </Text>
+    </WalletButton>
+  );
+};
+
+const WalletCard: React.FC<Props> = ({ login, walletConfig, onDismiss }) => {
   const { title, icon: Icon } = walletConfig;
   return (
-    <Button
-      width="100%"
+    <WalletButton
       variant="tertiary"
       onClick={() => {
         login(walletConfig.connectorId);
         window.localStorage.setItem(connectorLocalStorageKey, walletConfig.connectorId);
         onDismiss();
       }}
-      style={{ justifyContent: "space-between" }}
-      mb={mb}
       id={`wallet-connect-${title.toLocaleLowerCase()}`}
     >
-      <Text bold color="primary" mr="16px">
+      <Icon width="40px" />
+      <Text bold color="textSubtle" fontSize="14px">
         {title}
       </Text>
-      <Icon width="32px" />
-    </Button>
+    </WalletButton>
   );
 };
 
