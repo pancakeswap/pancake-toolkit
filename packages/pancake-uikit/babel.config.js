c34eee56dd0ffeaf29f3dcdfd73d55c7ed862398
hachiojidev <hachiojidev@protonmail.com>
2021-08-09 15:23:38 +0900

    build: Bump dependencies

--- packages/pancake-uikit/babel.config.js
+++ packages/pancake-uikit/babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   presets: ["@babel/preset-env"],
-  plugins: ["styled-components"],
+  plugins: [
+    "styled-components",
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
+    ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
 };
