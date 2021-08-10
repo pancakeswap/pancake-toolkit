f7cc5a9b3c0088279b6f305736b5f8bdb22f9f23
Nika Partsvania <nika.partsvania22@gmail.com>
2021-08-10 15:23:28 +0400

    swapped colors

--- packages/pancake-uikit/src/theme/base.ts
+++ packages/pancake-uikit/src/theme/base.ts
@@ -21,11 +21,11 @@ const mediaQueries: MediaQueries = {
 
 export const shadows = {
   level1: "0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)",
-  active: "0px 0px 0px 1px #0098A1, 0px 0px 4px 8px rgba(31, 199, 212, 0.4)",
-  success: "0px 0px 0px 1px #31D0AA, 0px 0px 0px 4px rgba(49, 208, 170, 0.2)",
-  warning: "0px 0px 0px 1px #ED4B9E, 0px 0px 0px 4px rgba(237, 75, 158, 0.2)",
-  focus: "0px 0px 0px 1px #7645D9, 0px 0px 0px 4px rgba(118, 69, 217, 0.6)",
-  inset: "inset 0px 2px 2px -1px rgba(74, 74, 104, 0.1)",
+  active: "0px 0px 0px 1px #26C0C9, 0px 0px 4px 8px rgba(31, 199, 212, 0.4)",
+  success: "0px 0px 0px 1px #00F5BA, 0px 0px 0px 4px rgba(49, 208, 170, 0.2)",
+  warning: "0px 0px 0px 1px #E6097A, 0px 0px 0px 4px rgba(237, 75, 158, 0.2)",
+  focus: "0px 0px 0px 1px #5402F7, 0px 0px 0px 4px rgba(118, 69, 217, 0.6)",
+  inset: "inset 0px 2px 2px -1px rgba(232, 214, 255, 0.1)",
 };
 
 const spacing: Spacing = [0, 4, 8, 16, 24, 32, 48, 64];
