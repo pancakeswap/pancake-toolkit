bc2e8de4cb43acec60f2b6daca089905bfd933da
ChefCorgi <corgi.farmer@gmail.com>
2021-09-23 18:15:24 +0200

    fix: Lint

--- packages/pancake-uikit/src/components/Timeline/styles.ts
+++ packages/pancake-uikit/src/components/Timeline/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { lightColors } from "../..";
+import { lightColors } from "../../theme";
 
 export const TimelineContainer = styled.ul`
   display: flex;
