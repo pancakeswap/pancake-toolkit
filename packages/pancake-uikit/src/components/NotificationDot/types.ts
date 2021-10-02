fd510210df595ccaf67590dbb7cbcae4c7973a1f
Chef Cheems <chefcheems@protonmail.com>
2021-10-02 14:34:06 +0300

    feat: Add notification dot for menu items with status on mobile

--- packages/pancake-uikit/src/components/NotificationDot/types.ts
+++ packages/pancake-uikit/src/components/NotificationDot/types.ts
@@ -1,8 +1,12 @@
+import { Colors } from "../..";
+
 export interface NotificationDotProps {
   show?: boolean;
+  color?: keyof Colors;
   children: React.ReactElement | React.ReactElement[];
 }
 
 export interface DotProps {
   show: boolean;
+  color: keyof Colors;
 }
