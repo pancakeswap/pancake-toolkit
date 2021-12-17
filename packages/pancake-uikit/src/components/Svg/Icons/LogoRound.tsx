8137e91e8bc7116a017e2f4f0e0d130b8f0ba439
Chef JoJo <94336009+chef-jojo@users.noreply.github.com>
2021-12-17 11:27:52 +0800

    fix(uikit): Bug on svg linear gradient

--- packages/pancake-uikit/src/components/Svg/Icons/LogoRound.tsx
+++ packages/pancake-uikit/src/components/Svg/Icons/LogoRound.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 96 96" {...props}>
-      <circle cx={48} cy={48} r={48} fill="url(#paint0_linear_10493:36952)" />
+      <circle cx={48} cy={48} r={48} fill="url(#paint0_linear_10493)" />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
@@ -25,7 +25,7 @@ const Icon: React.FC<SvgProps> = (props) => {
         fill="#633001"
       />
       <defs>
-        <linearGradient id="paint0_linear_10493:36952" x1={48} y1={0} x2={48} y2={96} gradientUnits="userSpaceOnUse">
+        <linearGradient id="paint0_linear_10493" x1={48} y1={0} x2={48} y2={96} gradientUnits="userSpaceOnUse">
           <stop stopColor="#53DEE9" />
           <stop offset={1} stopColor="#1FC7D4" />
         </linearGradient>
