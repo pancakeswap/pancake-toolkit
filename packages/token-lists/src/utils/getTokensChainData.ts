a71fb30c70ec6f2ede7eacd6fde98563a6febbb6
Chef Momota <chefmomota@chefmomotas-MacBook-Pro.local>
2022-03-10 17:47:07 +0800

    fix: Multicall limit call

--- packages/token-lists/src/utils/getTokensChainData.ts
+++ packages/token-lists/src/utils/getTokensChainData.ts
@@ -8,6 +8,15 @@ const rawLists = {
   "pcs-mini-extended": rawMiniExtended,
 };
 
+function sliceIntoChunks(arr: Array<any>, chunkSize: number) {
+  const res = [];
+  for (let i = 0; i < arr.length; i += chunkSize) {
+      const chunk = arr.slice(i, i + chunkSize);
+      res.push(chunk);
+  }
+  return res;
+}
+
 const getTokensChainData = async (listName: string, addressArray?: string[]): Promise<any[]> => {
   const isTest = addressArray && addressArray.length > 0;
   const tokens = isTest ? addressArray : rawLists[listName];
@@ -15,29 +24,38 @@ const getTokensChainData = async (listName: string, addressArray?: string[]): Pr
     console.error("No raw address list found");
     return [];
   }
-  const tokenInfoCalls = tokens.flatMap((address) => [
-    {
+  
+  const chunkSize = 1200
+  const chunkArray = tokens.length >= chunkSize ? sliceIntoChunks(tokens, chunkSize) : [tokens]
+
+  const tokensWithChainData = []
+  for (const index in chunkArray) {
+    const tokenInfoCalls = chunkArray[index].flatMap((address) => [
+      {
+        address,
+        name: "symbol",
+      },
+      {
+        address,
+        name: "name",
+      },
+      {
+        address,
+        name: "decimals",
+      },
+    ]);
+    const tokenInfoResponse = await multicallv2(erc20, tokenInfoCalls);
+    const data = chunkArray[index].map((address, i) => ({
+      name: tokenInfoResponse[i * 3 + 1][0],
+      symbol: tokenInfoResponse[i * 3][0],
       address,
-      name: "symbol",
-    },
-    {
-      address,
-      name: "name",
-    },
-    {
-      address,
-      name: "decimals",
-    },
-  ]);
-  const tokenInfoResponse = await multicallv2(erc20, tokenInfoCalls);
-  const tokensWithChainData = tokens.map((address, i) => ({
-    name: tokenInfoResponse[i * 3 + 1][0],
-    symbol: tokenInfoResponse[i * 3][0],
-    address,
-    chainId: 56,
-    decimals: tokenInfoResponse[i * 3 + 2][0],
-    logoURI: `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`,
-  }));
+      chainId: 56,
+      decimals: tokenInfoResponse[i * 3 + 2][0],
+      logoURI: `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`,
+    }));
+    tokensWithChainData.push(...data)
+  }
+
   if (!isTest) {
     const tokenListPath = `${path.resolve()}/src/tokens/${listName}.json`;
     const stringifiedList = JSON.stringify(tokensWithChainData, null, 2);
